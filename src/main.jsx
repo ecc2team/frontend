ca21ecdf957043893f7746e5c18719482ea91f95
이시은 <selee6047@gmail.com>
2026-08-05 01:14:34 +0900

    msw 실행으로 인해 화면이 사라져 main.jsx 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function enableMocking() {
+  if (!import.meta.env.DEV) {
+    return;
+  }
+
+  const { worker } = await import("./mocks/browser.js");
+
+  await worker.start({
+    onUnhandledRequest: "bypass",
+  });
+}
+
+async function bootstrap() {
+  try {
+    await enableMocking();
+    console.log("MSW 시작 성공");
+  } catch (error) {
+    console.error("MSW 시작 실패:", error);
+  }
+
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
+
+bootstrap();
